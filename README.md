f31552fbda1203e39a1f16286a4c9e82f8be352e
yagawa <yagawa@qzc.co.jp>
2019-11-06 11:34:38 +0900

    Add message "not exist emulator"

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Android Emulator Util
 
 ## start android emulator
-
 ```
+$ chmod +rx start-emu.sh list.sh
 $ ./start-emu.sh
     #select index of device
 ```
--- list.sh
+++ list.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-ARR=(`$ANDROID_HOME/emulator/emulator -list-avds`)
+E_PATH="$ANDROID_HOME/emulator/emulator"
+if test -e $E_PATH; then
+  CMD="$E_PATH -list-avds"
+else
+  echo "Colud not find Emulator"
+  exit 1
+fi
+
+ARR=$($CMD)
+
 I=0
 for v in ${ARR[@]}; do
   echo "${I} ${v}"
   I=$((${I}+1))
 done
-
